ec7b6f4f2ff772f97ee084c97793ea269a92839a
Seil Cho <seilcho7@hotmail.com>
2019-02-22 13:37:40 -0500

    added background music and sound effect when catching monster

--- catch_monster.py
+++ catch_monster.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+import math
 
 # Character class sets initial position and movement of hero and monster
 class Character():
@@ -8,6 +9,14 @@ class Character():
         self.y = y
         self.direction_x = 0
         self.direction_y = 0
+
+    # Calculate distance
+    def distance(self, char):
+        if math.sqrt((self.x - char.x)**2 + ((self.y - char.y))**2) < 32:
+            if type(char) == Monster:
+                char.show = False
+                
+
 
     # Monsters randomly move around, and as they hit end of the screen,
     # they come back from other side of the screen
@@ -30,8 +39,6 @@ class Monster(Character):
     # Randomly change speed and direction of the monsters
     def change_direction(self, number, speed):
         # Change direction to north
-        print(speed, "speed")
-        print(number, "number")
         if number == 0:
             self.direction_y = speed
             self.direction_y = -self.direction_y
@@ -63,13 +70,26 @@ class Hero(Character):
             self.y += direction_y
         if direction_y == -3:
             self.y += direction_y
-
     
 
 def main():
     width = 512
     height = 480
     blue_color = (97, 159, 182)
+    
+     # Set background music
+    pygame.mixer.init()
+    music = pygame.mixer.Sound('sounds/music.wav')
+    music.set_volume(0.3)
+    
+    # Set win sound effect
+    win = pygame.mixer.Sound('sounds/win.wav')
+    win.set_volume(1.0)
+
+    win_sound = 0
+    
+    music.play()
+
 
     pygame.init()
     screen = pygame.display.set_mode((width, height))
@@ -88,17 +108,16 @@ def main():
     # Game initialization
     # Set initial location of hero
     hero = Hero(240, 224)
+    hero.show = True
     
-    # Set initial location of monster
+    # Set initial location of monsters
     monster = Monster(241, 112)
-    # monster_two = Character(241, 112)
-    # monster_three = Character(241, 112)
-    # monster_four = Character(241, 112)
-    # monster_five = Character(241, 112)
-    # monster_six = Character(241, 112)
+    # monster_two = Monster(241, 112)
+    # monster_three = Monster(241, 112)
+    # monster_four = Monster(241, 112)
+    monster.show = True
 
     change_dir_countdown = 60
-
 
     stop_game = False
     while not stop_game:
@@ -123,44 +142,62 @@ def main():
                     hero.direction_x = 0
                 elif event.key == pygame.K_RIGHT:
                     hero.direction_x = 0
-
-            # Event handling
-
-            if event.type == pygame.QUIT:
-                stop_game = True
-
         
+        # Stops hero before bush
+        if hero.x <= 32:
+            hero.x = 32
+        if hero.x >= 448:
+            hero.x = 448
+        if hero.y <= 32:
+            hero.y = 32
+        if hero.y >= 416:
+            hero.y = 416
+
+
+        # Event handling
+
+        if event.type == pygame.QUIT:
+            stop_game = True
+
+        hero.distance(monster)
 
         # Game logic
         hero.move(hero.direction_x, hero.direction_y)
         monster.move()
-        # monster_two.moving()
-        # monster_three.moving()
-        # monster_four.moving()
-        # monster_five.moving()
-        # monster_six.moving()
+        # monster_two.move()
+        # monster_three.move()
+        # monster_four.move()
+
         change_dir_countdown -= 1
         
         # When countdown hit 0, monster change to random direction and speed
         if change_dir_countdown == 0:
             monster.change_direction(random.randint(0, 3), random.randint(0, 5))
-        #     # monster_two.change_direction(random.randint(0, 3), random.randint(2, 5))
-        #     # monster_three.change_direction(random.randint(0, 3), random.randint(2, 5))
-        #     # monster_four.change_direction(random.randint(0, 3), random.randint(2, 5))
+            # monster_two.change_direction(random.randint(0, 3), random.randint(2, 5))
+            # monster_three.change_direction(random.randint(0, 3), random.randint(2, 5))
+            # monster_four.change_direction(random.randint(0, 3), random.randint(2, 5))
             change_dir_countdown = 60
 
         
         # Draw background
         screen.fill(blue_color)
         screen.blit(background_image, [0, 0])
-        screen.blit(hero_image, [hero.x, hero.y])
-        screen.blit(monster_image, [monster.x, monster.y])
-        # screen.blit(monster_image, [monster_two.x, monster_two.y])
-        # screen.blit(monster_image, [monster_three.x, monster_three.y])
-        # screen.blit(monster_image, [monster_four.x, monster_four.y])
-        # screen.blit(monster_image, [monster_five.x, monster_five.y])
-        # screen.blit(monster_image, [monster_six.x, monster_six.y])
         
+        # Show hero as long as it is alive
+        if hero.show == True:
+            screen.blit(hero_image, [hero.x, hero.y])    
+
+        # Show monsters as long as hero does not catch
+        if monster.show == True:        
+            screen.blit(monster_image, [monster.x, monster.y])
+            # screen.blit(monster_image, [monster_two.x, monster_two.y])
+            # screen.blit(monster_image, [monster_three.x, monster_three.y])
+            # screen.blit(monster_image, [monster_four.x, monster_four.y])
+        else:
+            if win_sound == 0:
+                win.play()
+                win_sound = 1
+
         # Game display
         pygame.display.update()
         clock.tick(60)
